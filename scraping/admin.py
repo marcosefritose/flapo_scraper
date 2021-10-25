64ff0e0dab3cc0b267416e552a9a7e8fb54a08c9
Marc Gehrke <m.gehrke@business-unicorns.de>
2021-10-25 21:05:51 +0200

    added platforms to admin ui

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Review
+from .models import Review, Platform
 
 
 # Register your models here.
 admin.site.register(Review)
+admin.site.register(Platform)
